72a7a015ac3720aec21d813390e67658aa2b9733
artifact911 <artifact911@gmail.com>
2022-11-04 00:01:34 +0300

    entity done

--- src/main/java/org/art/HibernateRunner.java
+++ src/main/java/org/art/HibernateRunner.java
@@ -1,9 +1,13 @@
 package org.art;
 
 import lombok.SneakyThrows;
+import org.art.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
+
+import java.time.LocalDate;
 
 public class HibernateRunner {
 
@@ -12,12 +16,31 @@ public class HibernateRunner {
 
         // Этот класс служит для создания SessionFactory
         Configuration configuration = new Configuration();
+
+        // преобразовать название колонок из java-нэйминга в sql(xx_xxx)
+        // не юзаем. Заюзали анноташку над полем
+//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+
+        // указали, что это сущность и за ней надо теперь следить
+        // не стали юзать так, т.к. указали маппинг в xml
+//        configuration.addAnnotatedClass(User.class);
+
         // configure() принимает путь к xml. Если не указывать - ищет в рутовых ресурсах проекта
         configuration.configure();
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {   // теперь у SF мы можем получить сессию - что-то вроде обертки вокруг нашего Connection
-            System.out.println("ok");
+
+            session.beginTransaction();
+            User user = User.builder()
+                    .username("ivan@gmail.com")
+                    .firstname("Ivan")
+                    .lastname("Ivanov")
+                    .birthDate(LocalDate.of(2000, 1, 19))
+                    .age(20)
+                    .build();
+            session.save(user);
+            session.getTransaction().commit();
         }
     }
 }
---
+++ src/main/java/org/art/entity/User.java
@@ -0,0 +1,29 @@
+package org.art.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users", schema = "public") // схема тут не обязательно, тк эта по умолчанию
+public class User {
+
+    @Id
+    private String username;
+    private String firstname;
+    private String lastname;
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+    private Integer age;
+}
