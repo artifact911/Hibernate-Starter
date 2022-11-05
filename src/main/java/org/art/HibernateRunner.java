ba0decbf10692e93bd1fc5bf2c006df6d273f446
artifact911 <artifact911@gmail.com>
2022-11-05 23:21:44 +0300

    EntityPersister, Cache1lvl, EntityLifecycle done

--- src/main/java/org/art/HibernateRunner.java
+++ src/main/java/org/art/HibernateRunner.java
@@ -1,69 +1,45 @@
 package org.art;
 
 import lombok.SneakyThrows;
-import org.art.converter.BirthdayConverter;
-import org.art.entity.Birthday;
-import org.art.entity.Role;
 import org.art.entity.User;
+import org.art.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
-import java.time.LocalDate;
 
 public class HibernateRunner {
 
     @SneakyThrows
     public static void main(String[] args) {
 
-        // Этот класс служит для создания SessionFactory
-        Configuration configuration = new Configuration();
+        // тут этот user находится в transient для двух сессий.
+        User user = User.builder()
+                .username("ivan123@gmail.com")
+                .lastname("Ivanov")
+                .firstname("Ivan")
+                .build();
 
-        // преобразовать название колонок из java-нэйминга в sql(xx_xxx)
-        // не юзаем. Заюзали анноташку над полем
-//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
 
-        // указали, что это сущность и за ней надо теперь следить
-        // не стали юзать так, т.к. указали маппинг в xml
-//        configuration.addAnnotatedClass(User.class);
+        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
+             Session session1 = sessionFactory.openSession()) {
+            session1.beginTransaction();
 
-        // указали Хиберу, автоматом юзать этот конвертер. Вторым параметров нужно указать true, чтоб работало автоматом
-        // и не нужно было ставить над полями, а можно тут не писать, а поставить анноташку над конвертером
-        // @Converter(autoApply = true)
-        configuration.addAttributeConverter(new BirthdayConverter());
+            // теперь user в persistent для session1
+            session1.persist(user);
 
-        // configure() принимает путь к xml. Если не указывать - ищет в рутовых ресурсах проекта
-        configuration.configure();
+            session1.getTransaction().commit();
+        }
 
-        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
-             Session session = sessionFactory.openSession()) {   // теперь у SF мы можем получить сессию - что-то вроде обертки вокруг нашего Connection
+        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
+             Session session2 = sessionFactory.openSession()) {
+            session2.beginTransaction();
 
-            session.beginTransaction();
+            user.setFirstname("Sveta");
 
-//            User user = User.builder()
-//                    .username("ivan112@gmail.com")
-//                    .firstname("Ivan")
-//                    .lastname("Ivanov")
-//                    .info("""
-//                            {
-//                            "name": "Ivan",
-//                            "id": 25
-//                            }
-//                            """)
-//                    .birthDate(new Birthday(LocalDate.of(2000, 1, 19)))
-//                    .role(Role.ADMIN)
-//                    .build();
+            // тут наша сущность не с контексте еще нашей сесии, а значит хибер выполнит селект в БД и обновит user
+            // до стояния user БД (имя Светы сотрется)
+            session2.merge(user);
 
-            /**
-             * Операции persist, merge, remove осуществляются по id юзера
-             */
-//             session.persist(user);  // Добавляет пользователя в базу данных
-//             session.merge(user);  // Делает update юзера в БД. Если в БД такого юзера нет, то метод его туда запишет (saveOrUpdate)
-//             session.remove(user); // Удаляет юзера из БД
-
-            User user = session.get(User.class, "ivan112@gmail.com");
-
-            session.getTransaction().commit();
+            session2.getTransaction().commit();
         }
     }
 }
---
+++ src/main/java/org/art/util/HibernateUtil.java
@@ -0,0 +1,34 @@
+package org.art.util;
+
+import lombok.experimental.UtilityClass;
+import org.art.converter.BirthdayConverter;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+@UtilityClass
+public class HibernateUtil {
+
+    public static SessionFactory buildSessionFactory() {
+
+        // Этот класс служит для создания SessionFactory
+        Configuration configuration = new Configuration();
+
+        // преобразовать название колонок из java-нэйминга в sql(xx_xxx)
+        // не юзаем. Заюзали анноташку над полем
+//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+
+        // указали, что это сущность и за ней надо теперь следить
+        // не стали юзать так, т.к. указали маппинг в xml
+//        configuration.addAnnotatedClass(User.class);
+
+        // указали Хиберу, автоматом юзать этот конвертер. Вторым параметров нужно указать true, чтоб работало автоматом
+        // и не нужно было ставить над полями, а можно тут не писать, а поставить анноташку над конвертером
+        // @Converter(autoApply = true)
+        configuration.addAttributeConverter(new BirthdayConverter());
+
+        // configure() принимает путь к xml. Если не указывать - ищет в рутовых ресурсах проекта
+        configuration.configure();
+
+        return configuration.buildSessionFactory();
+    }
+}
